ed3f9caafbaacf8e64e868cc07ff9213b5e29431
Akos Szabo <91012723+akossz01@users.noreply.github.com>
2023-04-19 11:42:05 +0300

    Speech to Text + Header Structure Fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './home/header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-
+import { SpeechToTextComponent } from './speech-to-text/speech-to-text.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SpeechToTextComponent,
   ],
   imports: [
     BrowserModule,
