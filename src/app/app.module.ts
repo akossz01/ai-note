3352ee22e8c134205eaa4a594423f1345eca17b6
912-Crisan-Andrei <andrei.ionut.crisan2@stud.ubbcluj.ro>
2023-04-20 21:24:40 +0300

    input_component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ChatInputComponent } from './create-page/chat-input/chat-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatInputComponent
   ],
   imports: [
     BrowserModule
