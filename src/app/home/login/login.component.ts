9d7277cf19865fa3337772e3d0060d769d6f90f1
Izsak Andrea <izsakandrea1235@gmail.com>
2023-04-21 22:05:24 +0300

    angular material

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
-  selector: 'app-login',
+  selector: 'app-root',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class AppComponent {
+  title = 'demoApp';
+ email!:string;
+ password!: string;
+ remail!:string;
+ rpassword!:string;
+ rcpassword!:string;
 
+  constructor(private snackBar:MatSnackBar){
+
+  }
+  register() {
+
+  }
+  login() {
+    if(this.email=="admin" && this.password=="admin"){
+        this.snackBar.open('Login Successful','',{duration:1000})
+    }else{
+      this.snackBar.open('Login error','',{duration:1000})
+    }
+  }
 }
