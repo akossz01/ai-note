1191fc29c7582893c3b741e658e873788719e0f2
Marginean Cristian <cristianadrian1999@yahoo.com>
2023-04-21 13:58:20 +0300

    Output-work

--- src/app/speech-to-text/speech-to-text.component.ts
+++ src/app/speech-to-text/speech-to-text.component.ts
@@ -1,19 +1,61 @@
-import { Component, OnInit, Renderer2, ElementRef } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef, Output } from '@angular/core';
 import { SpeechRecognitionService } from '../services/speech-recognition.service'
+import { ChatGptServiceComponent } from '../services/chat-gpt-service/chat-gpt-service.component';
+
+/*
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+*/
+
+
+
+
 
 @Component({
   selector: 'app-speech-to-text',
   templateUrl: './speech-to-text.component.html',
   styleUrls: ['./speech-to-text.component.css'],
 
-  providers: [SpeechRecognitionService]
+  providers: [SpeechRecognitionService,
+  
+  ]
 })
-export class SpeechToTextComponent{
-  constructor(public service : SpeechRecognitionService, private renderer: Renderer2, private el: ElementRef) { 
-    this.service.init()
+export class SpeechToTextComponent implements OnInit{
+
+  //------------ Output
+  public response: string =''; // Inputul utilizatorului
+  output: string ='';
+
+  onSubmit(){
+    this.chatGptService.completePrompt(this.response).subscribe(
+      response => {
+        this.output = response;
+       /* this.output = response.choices[0]?.text || '';*/
+      },
+      error => {
+        console.error('Error at ChatGPT respone', error);
+      }
+      
+    );
   }
 
+  onInputChange(event: any){
+
+    this.response = event.target.value;
+
+  }
+
+  //---------Output
+
+  constructor(public service : SpeechRecognitionService, private renderer: Renderer2, private el: ElementRef,  private chatGptService: ChatGptServiceComponent) { 
+    this.service.init()
+  }
+  
+
   ngOnInit(): void {
+    /*
+    this.response = 'Initial value';
+    */
   }
 
   startService() {
