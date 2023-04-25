d5e3356db1dd616a9b5680e33e0f1f0ec1a24bcc
Marginean Cristian <cristianadrian1999@yahoo.com>
2023-04-25 16:34:47 +0300

    GenImg

--- src/app/services/chat-gpt-service/chat-gpt-service.component.ts
+++ src/app/services/chat-gpt-service/chat-gpt-service.component.ts
@@ -29,7 +29,7 @@ export class ChatGptServiceComponent {
     const headers = {
       'Content-Type': 'application/json',
       /*'Authorization':'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' // Înlocuiește API_KEY cu cheia ta de API de la OpenAI*/
-      'Authorization': 'Bearer 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' // Înlocuiește API_KEY cu cheia ta de API de la OpenAI
+      'Authorization': 'Bearer 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' // Înlocuiește API_KEY cu cheia ta de API de la OpenAI
 
     };
 
